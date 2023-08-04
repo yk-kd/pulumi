5e7b14dbb39b78c01d7989a5abd59f370b767216
Anton Tayanovskyy <anton@pulumi.com>
2023-08-04 11:47:06 -0400

    Rename magic field from 'resource' to 'res'

--- pkg/codegen/go/gen.go
+++ pkg/codegen/go/gen.go
@@ -2765,7 +2765,7 @@ func (pkg *pkgContext) genMethod(resourceName string, method *schema.Method, w i
 		if doReturnPlainType {
 			properties = []*schema.Property{
 				{
-					Name:  "resource",
+					Name:  "res",
 					Type:  returnPlainType,
 					Plain: true,
 				},
