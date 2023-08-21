13c15660f842e3ad5fab0794244a7262bb8ca8f4
Anton Tayanovskyy <anton@pulumi.com>
2023-08-21 19:44:54 -0400

    Fix lint

--- pkg/codegen/nodejs/gen.go
+++ pkg/codegen/nodejs/gen.go
@@ -2789,7 +2789,7 @@ func (mod *modContext) genUtilitiesFile(w io.Writer) error {
 		code = strings.ReplaceAll(code, "/*pluginDownloadURL*/", "")
 	}
 
-	_, err = fmt.Fprintf(w, code)
+	_, err = fmt.Fprintf(w, "%s", code)
 	return err
 }
 
